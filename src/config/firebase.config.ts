794deb1b9523c5e43155c940b783fe4ff21ed7f4
HCA31 <hochian31@gmail.com>
2024-10-22 18:12:18 +0700

    update firebase

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -4,12 +4,16 @@ import { config } from 'dotenv';
 // Load environment variables from .env file
 config();
 
+const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG!);
+firebaseConfig.private_key = firebaseConfig.private_key.replace(/\\n/g, '\n');
+
 // Initialize Firebase Admin SDK
 admin.initializeApp({
-  credential: admin.credential.cert(
-    join(__dirname, '../config/firebase-service-account.json'),
-  ),
-  //   credential: admin.credential.cert(serviceAccount),
+  // credential: admin.credential.cert(
+  //   join(__dirname, '../config/firebase-service-account.json'),
+  // ),
+  credential: admin.credential.cert(firebaseConfig),
+
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET, // Lưu tên bucket trong file .env
 });
 
